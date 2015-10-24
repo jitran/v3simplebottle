9fa3c38349bad35e7afacadbfb7ee237c43fbc2f
jitran <jitran@users.noreply.github.com>
2015-10-25 10:26:00 +1100

    Updated with hostname in the output

--- app.py
+++ app.py
@@ -2,10 +2,11 @@ __author__ = 'spousty'
 
 
 from bottle import route, run
+import socket
 
 @route('/')
 def index():
-    return "<h1> hello OpenShift Ninja</h1>"
+    return "<h1>[%]: hello OpenShift Ninja</h1>" % (socket.gethostname())
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
