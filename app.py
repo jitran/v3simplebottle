662a4b10443a342782da9239d61a5ab07226e7be
jitran <jitran@users.noreply.github.com>
2015-10-25 10:30:11 +1100

    Fixed typo

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ import socket
 
 @route('/')
 def index():
-    return "<h1>[%]: hello OpenShift Ninja</h1>" % (socket.gethostname())
+    return "<h1>[%s]: hello OpenShift Ninja</h1>" % (socket.gethostname())
 
 if __name__ == '__main__':
     run(host='0.0.0.0', port=8080)
